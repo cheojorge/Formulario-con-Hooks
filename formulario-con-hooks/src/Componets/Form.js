01b9d08c03c913cbba04dac395f03fd52401cf6e
cheojorge <cheojorge@hotmail.com>
2022-01-14 23:25:28 -0500

    Finish

---
+++ formulario-con-hooks/src/Componets/Form.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+
+const Form = props => {
+    const [firstName , setFirstName ] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");  
+    const [passwordConfirm, setPasswordConfirm] = useState("");  
+
+    return(
+        <>
+            <form>
+                <div className="mb-3 row bg-light">
+                    <label htmlFor="inputFirstName" className="col-sm-2 col-form-label">First Name</label>
+                    <div className="col-sm-10">
+                        <input type="text" className="form-control" id="inputFirstName" onChange={(e)=>setFirstName(e.target.value)}/>
+                    </div>
+                </div>
+                <div className="mb-3 row bg-light">
+                    <label htmlFor="inputLastName" className="col-sm-2 col-form-label">Last Name</label>
+                    <div className="col-sm-10">
+                        <input type="text" className="form-control" id="inputLastName" onChange={(e)=>setLastName(e.target.value)}/>
+                    </div>
+                </div>
+                <div className="mb-3 row bg-light">
+                    <label htmlFor="inputEmail" className="col-sm-2 col-form-label">Email</label>
+                    <div className="col-sm-10">
+                        <input type="text" className="form-control" id="inputEmail" onChange={(e)=>setEmail(e.target.value)}/>
+                    </div>
+                </div>
+                <div className="mb-3 row bg-light">
+                    <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Password</label>
+                    <div className="col-sm-10">
+                        <input type="password" className="form-control" id="inputPassword" onChange={(e)=>setPassword(e.target.value)}/>
+                    </div>
+                </div>
+                <div className="mb-3 row bg-light">
+                    <label htmlFor="inputConfirmPassword" className="col-sm-2 col-form-label">Confirm Password</label>
+                    <div className="col-sm-10">
+                        <input type="password" className="form-control" id="inputConfirmPassword" onChange={(e)=>setPasswordConfirm(e.target.value)}/>
+                    </div>
+                </div>
+            </form>
+            <table className="table-borderless">
+                <thead>
+                    <tr>
+                        <th scope="col"></th>
+                        <th scope="col">Your Form Data</th>
+                        
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Fist Name</td>
+                        <td>{firstName}</td>
+                    </tr>
+                    <tr>
+                        <td>Last Name</td>
+                        <td>{lastName}</td>
+                    </tr>
+                    <tr>
+                        <td>Email</td>
+                        <td>{email}</td>
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td>{password}</td>
+                    </tr>
+                    <tr>
+                        <td>Confirm Password</td>
+                        <td>{passwordConfirm}</td>
+                    </tr>
+                </tbody>
+                </table>
+            
+        </>
+       
+    )
+}
+
+export default Form;
